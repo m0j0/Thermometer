56fa0579d1e079ca90474d6fb1e09b5e0db68a72
m0j0 <maslovn@live.com>
2016-01-03 17:31:33 +0700

    add mvvm

--- UniversalApp/Thermometer.Shared/App.xaml.cs
+++ UniversalApp/Thermometer.Shared/App.xaml.cs
@@ -15,6 +15,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using MugenMvvmToolkit;
+using MugenMvvmToolkit.WinRT.Infrastructure;
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
 
@@ -45,16 +47,17 @@ namespace Thermometer
         /// search results, and so forth.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
         {
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
+//#if DEBUG
+//            if (System.Diagnostics.Debugger.IsAttached)
+//            {
+//                this.DebugSettings.EnableFrameRateCounter = true;
+//            }
+//#endif
 
             Frame rootFrame = Window.Current.Content as Frame;
+            WinRTBootstrapperBase bootstrapper = null;
 
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
@@ -62,6 +65,8 @@ namespace Thermometer
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
+                bootstrapper = new Bootstrapper<ViewModels.App>(rootFrame, new AutofacContainer());
+                await bootstrapper.InitializeAsync();
 
                 // TODO: change this value to a cache size that is appropriate for your application
                 rootFrame.CacheSize = 1;
@@ -95,10 +100,7 @@ namespace Thermometer
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
-                {
-                    throw new Exception("Failed to create initial page");
-                }
+                bootstrapper?.Start();
             }
 
             // Ensure the current window is active
