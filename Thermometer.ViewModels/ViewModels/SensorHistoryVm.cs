6b44221c0cee7441b63c6d11437e37087bdb446d
m0j0 <maslovn@live.com>
2016-01-07 18:52:48 +0700

    add chart control

--- Thermometer.ViewModels/ViewModels/SensorHistoryVm.cs
+++ Thermometer.ViewModels/ViewModels/SensorHistoryVm.cs
@@ -18,11 +18,20 @@ namespace Thermometer.ViewModels
                 Tuple.Create(SensorHistoryPeriod.Week, SensorHistoryPeriod.Week.ToText()),
                 Tuple.Create(SensorHistoryPeriod.Month, SensorHistoryPeriod.Month.ToText())
             };
+
+            var random = new Random();
+            Items = new List<Tuple<int, int>>();
+            for (int i = 0; i < 20; i++)
+            {
+                Items.Add(Tuple.Create(i, random.Next(0, 50)));
+            }
         }
 
         public IList<Tuple<SensorHistoryPeriod, string>> PeriodTypes { get; }
 
         public SensorHistoryPeriod SelectedPeriodType { get; set; }
+
+        public IList<Tuple<int, int>> Items { get; }
 
         protected override void OnInitialized()
         {
