ecb5451980985f35cca22c4a311e9950aa0f17f9
m0j0 <maslovn@gmail.com>
2016-01-21 17:11:29 +0700

    add wrapper

---
+++ Thermometer.ViewModels/Modules/ViewModelWrapperRegistrationModule.cs
@@ -0,0 +1,19 @@
+﻿using MugenMvvmToolkit.Interfaces;
+using MugenMvvmToolkit.Modules;
+using Thermometer.Interfaces;
+using Thermometer.ViewModels.Wrappers;
+
+namespace Thermometer.Modules
+{
+    public class ViewModelWrapperRegistrationModule : WrapperRegistrationModuleBase
+    {
+        #region Overrides of WrapperRegistrationModuleBase
+
+        protected override void RegisterWrappers(IConfigurableWrapperManager wrapperManager)
+        {
+            wrapperManager.AddWrapper<IDisplayWrapperVm, DisplayWrapperVm>();
+        }
+
+        #endregion
+    }
+}
---
+++ Thermometer.ViewModels/ViewModels/Wrappers/DisplayWrapperVm.cs
@@ -0,0 +1,10 @@
+﻿using MugenMvvmToolkit.Interfaces.ViewModels;
+using MugenMvvmToolkit.ViewModels;
+using Thermometer.Interfaces;
+
+namespace Thermometer.ViewModels.Wrappers
+{
+    public class DisplayWrapperVm : WrapperViewModelBase<IViewModel>, IDisplayWrapperVm
+    {
+    }
+}
